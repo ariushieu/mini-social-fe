44f51f08f8b18535e0ecb8e2bf3a3bb6d5aeff4c
hieu-nguyen <hieunguyen2005q@gmail.com>
2025-12-13 00:48:56 +0700

    update logic refresh token

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axiosConfig";
 import { type LoginRequest, type LoginResponse } from "../types/auth/AuthLogin";
 import {type RegisterRequest, type RegisterResponse } from "../types/auth/AuthRegister";
 
